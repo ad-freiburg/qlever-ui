5ff050b39aea836f37a80b918589d0df3dd0c6d3
jbuerklin <buerklij@informatik.uni-freiburg.de>
2018-12-15 13:58:33 +0100

    renamed suggest to api/suggest

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<backend>[A-Za-z0-9\+@:%\-_]+|)$', views.index, name='index'),
-    url(r'^suggest$', views.getSuggestions, name='getSuggestions'),
+    url(r'^(?P<backend>[A-Za-z0-9\+@:%\-_]*)$', views.index, name='index'),
+    url(r'^api/suggest$', views.getSuggestions, name='getSuggestions'),
 ]
