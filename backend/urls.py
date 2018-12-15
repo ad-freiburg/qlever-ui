f1ae934ae2ebfc2f9690fe7dcee79543bb75f09a
Daniel Kemen <dk@software-design.de>
2018-12-15 11:49:02 +0100

    - Added backend to url
    - Better detection of active backend
    - Error if no backend available

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^(?P<backend>[A-Za-z0-9\+@:%\-_]+|)$', views.index, name='index'),
     url(r'^suggest$', views.getSuggestions, name='getSuggestions'),
 ]
