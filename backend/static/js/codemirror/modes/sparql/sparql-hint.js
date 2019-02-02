521569d5e9f554490a93d0f16bb2d16717bbf44c
Daniel Kemen <daniel.kemen@saturn.uni-freiburg.de>
2019-02-02 13:15:07 +0100

    - Removed  / changed some text/logs

--- backend/static/js/codemirror/modes/sparql/sparql-hint.js
+++ backend/static/js/codemirror/modes/sparql/sparql-hint.js
@@ -731,8 +731,6 @@ function getAbsolutePosition(cur){
     
 **/    
 function getCurrentContext(absPosition,content,iteration){
-	log('Searching in','parsing')
-    log(content,'parsing')
     var foundContext = undefined;
     
     if(iteration >= 10){ return undefined; }
