86bcfc616f12501004955009c6b27efc1d856c8e
Daniel Kemen <daniel.kemen@saturn.uni-freiburg.de>
2019-02-17 15:21:17 +0100

    - Fixed bug with highlighting of language codes

--- backend/static/js/codemirror/modes/sparql/sparql.js
+++ backend/static/js/codemirror/modes/sparql/sparql.js
@@ -318,7 +318,7 @@ var COMPLEXTYPES = [
                 }
                 return "entity";
             }  else if (ch == "@") {
-                stream.match(/[\w-]*/);
+                stream.match(/[\w-]*@/);
                 return "string string-language";
             } else  {
 	            before = getBefore(stream, /:/);
