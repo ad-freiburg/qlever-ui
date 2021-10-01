18abe9938af129e3e5893eaf3b6c3171d0805b29
Hannah Bast <bast@informatik.uni-freiburg.de>
2021-10-01 02:49:53 +0200

    Add settings_secret.py to simplify installation
    The previous setup was more secure, but the QLever UI is not really a
    high-security applications, at least for now. Anyway, the key in
    settings_secret.py can be changed anytime if so desired. It's just
    easier to forget this now. In my understanding, the only reason for the
    previous setup was that one could not forget it because one needed to
    explicitly copy the file before building the docker image.

---
+++ qlever/settings_secret.py
@@ -0,0 +1,7 @@
+# Change these values and rename this file to "settings_secret.py"
+# SECURITY WARNING: If you are using this in production, do NOT use the default SECRET KEY!
+# See: https://docs.djangoproject.com/en/3.0/ref/settings/#std:setting-SECRET_KEY
+SECRET_KEY = '!!super_secret!!'
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['*']
