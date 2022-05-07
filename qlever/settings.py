df269b7a04aa215a15b3d82212015695caa30e0c
Hannah Bast <bast@cs.uni-freiburg.de>
2022-05-07 03:12:55 +0200

    Add CSRF_TRUSTED_ORIGINS (could not login or save anymore)

--- qlever/settings.py
+++ qlever/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://*.uni-freiburg.de']
+
 ROOT_URLCONF = 'qlever.urls'
 
 TEMPLATES = [
