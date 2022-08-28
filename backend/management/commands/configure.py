99fa52fe049d676e816934654b8a6bf2f3fda498
Hannah Bast <bast@cs.uni-freiburg.de>
2022-08-28 08:22:38 +0200

    Add second argument (URL) to configure command

--- backend/management/commands/configure.py
+++ backend/management/commands/configure.py
@@ -2,11 +2,11 @@ from django.core.management.base import BaseCommand, CommandError
 from backend.models import Backend
 
 class Command(BaseCommand):
-    help = ("Usage: python manage.py configure <backend slug>\n"
+    help = ("Usage: python manage.py configure <backend slug> <backend_url>\n"
             "\n"
-            "Make the specified backend the default and all other "
-            "backends invisible (but they are still there and can "
-            "be activated in the admin panel")
+            "Make the specified backend the default and set the specified URL."
+            "Mak all all other backends invisible (but they are still there "
+            "and can be edited at wish in the admin panel). ")
 
     # Copied from warmup.py, is this really needed?
     def __init__(self, *args, **kwargs):
@@ -20,14 +20,18 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("backend_slug", nargs=1,
                             help="Slug of the selected backend")
+        parser.add_argument("backend_url", nargs=1,
+                            help="URL of the selected backend")
 
     def handle(self, *args, returnLog=False, **options):
         backend_slug = options["backend_slug"][0]
+        backend_url = options["backend_url"][0]
 
-        self.log(f"Make backend {backend_slug} the default ...")
+        self.log(f"Make backend {backend_slug} the default and set URL to {backend_url} ...")
         try:
             backend = Backend.objects.filter(slug=backend_slug).get()
             backend.isDefault = True
+            backend.baseUrl = backend_url
             backend.sortKey = 1
             backend.save()
         except Exception as e:
