e87cb64d2469b1dadc71311fe5b96d50bf27a33e
RobinTF <83676088+RobinTF@users.noreply.github.com>
2023-12-11 01:43:25 +0100

    Run catch block if stats request is unsuccessful

--- backend/static/js/qleverUI.js
+++ backend/static/js/qleverUI.js
@@ -684,30 +684,31 @@ async function handleStatsDisplay() {
     try {
       const response = await fetch(`${BASEURL}?cmd=stats`);
       if (response.ok) {
-        const result = await response.json();
-        log('Evaluating and displaying stats...', 'other');
-        $("#kbname").html(result.kbindex ?? result["name-index"]);
-        if (result["name-text-index"]) {
-          $("#nrecords").html(tsep(result["num-text-records"] ?? result["nofrecords"]));
-          $("#nwo").html(tsep(result["num-word-occurrences"] ?? result["nofwordpostings"]));
-          $("#neo").html(tsep(result["num-entity-occurrences"] ?? result["nofentitypostings"]));
-          $("#textname").html(result["name-text-index"]);
-        } else {
-          $("#textname").closest("div").hide();
-        }
-        $("#ntriples").html(tsep(result["num-triples-normal"] ?? result["nofActualTriples"]))
-        $("#permstats").html(result["num-permutations"] ?? result["permutations"]);
-        if ((result["num-permutations"] ?? result["permutations"]) == "6") {
-          $("#kbstats").html("Number of subjects: <b>" +
-            tsep(result["num-subjects-normal"] ?? result["nofsubjects"]) + "</b><br>" +
-          "Number of predicates: <b>" +
-            tsep(result["num-predicates-normal"] ?? result["nofpredicates"]) + "</b><br>" +
-          "Number of objects: <b>" +
-            tsep(result["num-objects-normal"] ?? result["nofobjects"]) + "</b>");
-        }
-        $('#statsButton').removeAttr('disabled');
-        $('#statsButton span').html('Index Information');
+        throw new Error(`Server returned ${response.status} ${response.statusText}.`);
       }
+      const result = await response.json();
+      log('Evaluating and displaying stats...', 'other');
+      $("#kbname").html(result.kbindex ?? result["name-index"]);
+      if (result["name-text-index"]) {
+        $("#nrecords").html(tsep(result["num-text-records"] ?? result["nofrecords"]));
+        $("#nwo").html(tsep(result["num-word-occurrences"] ?? result["nofwordpostings"]));
+        $("#neo").html(tsep(result["num-entity-occurrences"] ?? result["nofentitypostings"]));
+        $("#textname").html(result["name-text-index"]);
+      } else {
+        $("#textname").closest("div").hide();
+      }
+      $("#ntriples").html(tsep(result["num-triples-normal"] ?? result["nofActualTriples"]))
+      $("#permstats").html(result["num-permutations"] ?? result["permutations"]);
+      if ((result["num-permutations"] ?? result["permutations"]) == "6") {
+        $("#kbstats").html("Number of subjects: <b>" +
+          tsep(result["num-subjects-normal"] ?? result["nofsubjects"]) + "</b><br>" +
+        "Number of predicates: <b>" +
+          tsep(result["num-predicates-normal"] ?? result["nofpredicates"]) + "</b><br>" +
+        "Number of objects: <b>" +
+          tsep(result["num-objects-normal"] ?? result["nofobjects"]) + "</b>");
+      }
+      $('#statsButton').removeAttr('disabled');
+      $('#statsButton span').html('Index Information');
     } catch (error) {
       log(error.message, 'requests');
       $('#statsButton span').html('<i class="glyphicon glyphicon-remove" style="color: red;"></i> Unable to connect to backend');
