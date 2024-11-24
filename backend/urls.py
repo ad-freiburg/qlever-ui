24c569400b9186c8a9ac107437eaca0090e4afcb
Hannah Bast <bast@cs.uni-freiburg.de>
2024-11-24 17:50:40 +0100

    Add command `prefixes` to obtain prefix definitions from backend config
    The new command can be called in two ways (taking `wikidata` as example
    backend config):
    
    python manage.py prefixes wikidata  [possibly within Docker]
    curl -s https://qlever.cs.uni-freiburg.de/api/prefixes/wikidata
    
    In both cases, the output is a text with all the prefix definitions
    defined for the requests backend, with one definition per line,
    using `PREFIX` (and not `@prefix`) and without trailing dot. For
    example:
    
    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
    PREFIX wikibase: <http://wikiba.se/ontology#>

--- backend/urls.py
+++ backend/urls.py
@@ -1,13 +1,19 @@
 from django.urls import re_path
-from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
-    re_path(r'^(?P<backend>[A-Za-z0-9\+@:()%\-_]*)(/(?P<short>[A-Za-z0-9]{6})?)?$',
+    re_path(
+        r"^(?P<backend>[A-Za-z0-9\+@:()%\-_]*)(/(?P<short>[A-Za-z0-9]{6})?)?$",
         views.index,
-        name='index'),
-    re_path(r'^api/share$', views.shareLink, name='shareLink'),
-    re_path(r'^api/warmup/(?P<backend>[^/]+)/(?P<target>[a-zA-Z0-9_-]+)$', views.warmup, name='warmup'),
-    re_path(r'^api/examples/(?P<backend>[^/]+)$', views.examples, name='examples'),
+        name="index",
+    ),
+    re_path(r"^api/share$", views.shareLink, name="shareLink"),
+    re_path(
+        r"^api/warmup/(?P<backend>[^/]+)/(?P<target>[a-zA-Z0-9_-]+)$",
+        views.warmup,
+        name="warmup",
+    ),
+    re_path(r"^api/examples/(?P<backend>[^/]+)$", views.examples, name="examples"),
+    re_path(r"^api/prefixes/(?P<backend>[^/]+)$", views.prefixes, name="prefixes"),
 ]
