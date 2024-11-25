24a43bf23c4abe1d506ad852fe19fea978e749f0
Ioannis Nezis <74186504+IoannisNezis@users.noreply.github.com>
2024-11-25 16:28:18 +0100

    Fix formatting quirks + migrate from `webpack` to `Rollup` (#117)
    The new version of `wasm-pack` does not work well with `webpack` (the current bundler), so we now take `Rollup` instead. The `wasm` module must now be loaded explicitly in `formatter/index.js`. On the side, fix some quirks of the formatter regarding CONSTRUCT and FROM.

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from '@rollup/plugin-node-resolve';
+import { wasm } from '@rollup/plugin-wasm';
+
+export default {
+	input: './formatter/index.js', // Entry point for your app
+	output: {
+		dir: './backend/static/wasm/formatter/',
+		format: 'esm',
+	},
+	plugins: [
+		resolve(),
+		wasm({
+			maxFileSize: 0, // Set to 0 to always load as separate files
+			publicPath: "/static/wasm/formatter/"
+		}
+		),
+	]
+};
