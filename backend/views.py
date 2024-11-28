ad-freiburg/qlever-ui

commit 19ebc1f73dd56e0957bcb0e86c66e89486a0def1

Add command `prefixes` to obtain prefix definitions from backend config (#118)
The new command can be called in two ways (taking `wikdata` as example backend config):
    
python manage.py prefixes wikidata  [possibly within Docker]
curl -s https://qlever.cs.uni-freiburg.de/api/prefixes/wikidata
    
In both cases, the output is a text with all the prefix definitions defined for the requests backend, with one definition per line, using `PREFIX` (and not `@prefix`) and without trailing dot, sorted alphabetically. For example, for Wikidata:

PREFIX cc: <http://creativecommons.org/ns#>
PREFIX data: <https://www.wikidata.org/wiki/Special:EntityData/>
PREFIX dct: <http://purl.org/dc/terms/>
...
PREFIX wdtn: <http://www.wikidata.org/prop/direct-normalized/>
PREFIX wikibase: <http://wikiba.se/ontology#>
PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
    
On the side, reformat some relevant files using `ruff`.

backend/views.py
@@ -1,14 +1,12 @@
-from django.core.management.base import CommandError
 from django.http.response import HttpResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.shortcuts import render
 
 from .models import *
 from backend.management.commands.warmup import Command as WarmupCommand
 from backend.management.commands.examples import Command as ExamplesCommand
+from backend.management.commands.prefixes import Command as PrefixesCommand
 
 import json
 import urllib
@@ -22,8 +20,8 @@ import datetime
 def index(request, backend=None, short=None):
     """
 
-        Index view - shows the UI with all available backends
-        If no preferred backend is set this view choses the default from the database
+    Index view - shows the UI with all available backends
+    If no preferred backend is set this view choses the default from the database
 
     """
     activeBackend = None
@@ -31,13 +29,12 @@ def index(request, backend=None, short=None):
     prefill = None
     noSlugMode = False
 
-    if request.POST.get('whitespaces', False):
-        request.session['logParsing'] = request.POST.get('logParsing', False)
-        request.session['logRequests'] = request.POST.get('logRequests', False)
-        request.session['logSuggestions'] = request.POST.get(
-            'logSuggestions', False)
-        request.session['logOther'] = request.POST.get('logOther', False)
-        request.session['whitespaces'] = request.POST['whitespaces']
+    if request.POST.get("whitespaces", False):
+        request.session["logParsing"] = request.POST.get("logParsing", False)
+        request.session["logRequests"] = request.POST.get("logRequests", False)
+        request.session["logSuggestions"] = request.POST.get("logSuggestions", False)
+        request.session["logOther"] = request.POST.get("logOther", False)
+        request.session["whitespaces"] = request.POST["whitespaces"]
 
     # if a backend is given try to activate it
     if backend:
@@ -56,27 +53,28 @@ def index(request, backend=None, short=None):
                     break
 
         if activeBackend is None:
-            return redirect('/')
+            return redirect("/")
 
     # if no backend is given activate the last one or a default with no-slug mode
     else:
         # go to the last active backend if set
-        if request.session.get('backend', False):
-            activeBackend = Backend.objects.filter(pk=request.session['backend']).first()
+        if request.session.get("backend", False):
+            activeBackend = Backend.objects.filter(
+                pk=request.session["backend"]
+            ).first()
         # find a default backend
         else:
-            activeBackend = Backend.objects.order_by('-isDefault').first()
+            activeBackend = Backend.objects.order_by("-isDefault").first()
 
         if activeBackend is not None:
             if activeBackend.isNoSlugMode:
                 noSlugMode = True
             else:
-                return redirect('/' + activeBackend.slugify())
+                return redirect("/" + activeBackend.slugify())
 
     if activeBackend is not None:
-
         # safe to session
-        request.session['backend'] = activeBackend.pk
+        request.session["backend"] = activeBackend.pk
 
         # Get examples ordered by `sortKey`.
         examples = Example.objects.filter(backend=activeBackend).order_by("sortKey")
@@ -89,59 +87,69 @@ def index(request, backend=None, short=None):
             # if no valid link, then redirect to main backend route
             if activeBackend is not None:
                 if activeBackend.isNoSlugMode:
-                    return redirect('/')
+                    return redirect("/")
                 else:
-                    return redirect('/' + activeBackend.slugify())
+                    return redirect("/" + activeBackend.slugify())
             else:
-                return redirect('/')
+                return redirect("/")
 
         prefill = link.content
     elif request.GET.get("query") is not None:
         prefill = request.GET["query"]
 
     return render(
-        request, 'index.html', {
-            'backend': activeBackend,
-            'noSlugMode': noSlugMode,
-            'prefixes': json.dumps(activeBackend.availablePrefixes) if activeBackend else '{}',
-            'backends': Backend.objects.all(),
-            'examples': examples,
-            'prefill': prefill
-        })
+        request,
+        "index.html",
+        {
+            "backend": activeBackend,
+            "noSlugMode": noSlugMode,
+            "prefixes": json.dumps(activeBackend.availablePrefixes)
+            if activeBackend
+            else "{}",
+            "backends": Backend.objects.all(),
+            "examples": examples,
+            "prefill": prefill,
+        },
+    )
 
 
 @csrf_exempt
 def shareLink(request):
     """
-        Generate a sharing link
+    Generate a sharing link
     """
 
-    if request.GET.get('cleanup') != 'true':
-        content = request.POST.get('content')
+    if request.GET.get("cleanup") != "true":
+        content = request.POST.get("content")
         link = Link.objects.filter(content=content).first()
         if not link:
             # space for 56.800.235.584 unique queries in history
             # asuming that one query is about 500 Bytes these are ~ 28 TB of history data
             # asuming that one query is about 1000 Bytes these are ~ 56 TB of history data
-            identifier = ''.join(
-                random.choice(string.ascii_lowercase + string.ascii_uppercase +
-                              string.digits) for _ in range(6))
+            identifier = "".join(
+                random.choice(
+                    string.ascii_lowercase + string.ascii_uppercase + string.digits
+                )
+                for _ in range(6)
+            )
             while Link.objects.filter(identifier=identifier).exists():
-                identifier = ''.join(
-                    random.choice(string.ascii_lowercase + string.ascii_uppercase +
-                                  string.digits) for _ in range(6))
+                identifier = "".join(
+                    random.choice(
+                        string.ascii_lowercase + string.ascii_uppercase + string.digits
+                    )
+                    for _ in range(6)
+                )
 
-            link = Link.objects.create(
-                identifier=identifier, content=content)
+            link = Link.objects.create(identifier=identifier, content=content)
 
         queryString = urllib.parse.urlencode({"query": content})
 
-        return JsonResponse({'link': link.identifier, "queryString": queryString})
+        return JsonResponse({"link": link.identifier, "queryString": queryString})
 
     else:
         if request.user.is_superuser:
             Link.objects.all().delete()
-        return redirect('/')
+        return redirect("/")
 
 
 # Handle "warmup" request.
@@ -167,30 +175,37 @@ def warmup(request, backend, target):
     try:
         tsv = command.handle(returnLog=True, backend=[backend], target=target)
     except Exception as e:
-        return JsonResponse({"status":"error", "message": str(e)})
+        return JsonResponse({"status": "error", "message": str(e)})
     return HttpResponse(tsv, content_type="text/tab-separated-values")
     # return JsonResponse({"status":"ok", "log": log})
 
-# Handle "examples" request, for example:
-#
-# With the URL https://qlever.cs.uni-freiburg.de/api/examples/wikidata
-#
-# this function is called with backend = "wikidata".
+
+# Handle API request like https://qlever.cs.uni-freiburg.de/api/examples/wikidata
 def examples(request, backend):
-    print_to_log(f"Call of \"examples\" in views.py with backend = \"{backend}\"")
+    print_to_log(f'Call of "examples" in views.py with backend = "{backend}"')
     command = ExamplesCommand()
     try:
-        tsv = command.handle(returnLog=True, slug=[backend])
+        examples_tsv = command.handle(returnLog=True, slug=[backend])
     except Exception as e:
         return HttpResponse("Error: " + str(e), status=500)
     return HttpResponse(tsv, content_type="text/tab-separated-values")
 
 
+# Handle API request like https://qlever.cs.uni-freiburg.de/api/prefixes/wikidata
+def prefixes(request, backend):
+    print_to_log(f'Call of "prefixes" in views.py with backend = "{backend}"')
+    command = PrefixesCommand()
+    try:
+        prefixes_text = command.handle(returnLog=True, slug=[backend])
+    except Exception as e:
+        return HttpResponse("Error: " + str(e), status=500)
+    return HttpResponse(prefixes_text, content_type="text/plain")
+
+
 # Helpers
 def print_to_log(msg, output=print):
     """
-        Helper to log things that happen during the process
+    Helper to log things that happen during the process
     """
-    logMsg = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S') + ' ' + str(
-        msg)
+    logMsg = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S") + " " + str(msg)
     output(logMsg)

backend/management/commands/prefixes.py
@@ -1,0 +1,49 @@
+from django.core.management.base import BaseCommand
+from backend.models import Backend
+import re
+
+
+# Command to get the PREFIX definitions from the given backend.
+class Command(BaseCommand):
+    help = "Usage: python manage.py examples <argument string>"
+
+    # Call the parent constructor.
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    # Log to the console.
+    def log(self, msg=""):
+        print(msg)
+
+    # Command line arguments.
+    def add_arguments(self, parser):
+        parser.add_argument("slug", nargs=1, help="Slug of the backend")
+
+    # This defines what the command does: it should return a string with one
+    # PREFIX definition per line.
+    def handle(self, *args, returnLog=False, **options):
+        # Get the slug from the command line arguments.
+        try:
+            slug = options["slug"][0]
+        except Exception as e:
+            self.log(f"Error parsing command line arguments: {e}")
+            self.log()
+            self.print_help("manage.py", "prefixes")
+            return
+        try:
+            backend = Backend.objects.filter(slug=slug).get()
+        except Exception as e:
+            self.log(f'Error finding config with slug "{slug}": {e}')
+            return
+        # Get the contents of the `suggestPrefixes` field. Replace `@prefix` by
+        # `PREFIX` at the beginning of each line and remove the `.` in the end
+        # if it is there. Also replace all whitespace by a single space and
+        # remove leading and trailing whitespace from each line.
+        prefix_defs = backend.suggestedPrefixes.split("\n")
+        for i, prefix_def in enumerate(prefix_defs):
+            prefix_def = re.sub(r"^@prefix", "PREFIX", prefix_def)
+            prefix_def = re.sub(r"\s*\.\s*$", "", prefix_def)
+            prefix_def = re.sub(r"\s+", " ", prefix_def)
+            prefix_def = prefix_def.strip()
+            prefix_defs[i] = prefix_def
+        return "\n".join(sorted(prefix_defs)) + "\n"

backend/urls.py
@@ -1,13 +1,19 @@
 from django.urls import re_path
-from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
-    re_path(r'^(?P<backend>[A-Za-z0-9\+@:()%\-_]*)(/(?P<short>[A-Za-z0-9]{6})?)?$',
+    re_path(
+        r"^(?P<backend>[A-Za-z0-9\+@:()%\-_]*)(/(?P<short>[A-Za-z0-9]{6})?)?$",
         views.index,
-        name='index'),
-    re_path(r'^api/share$', views.shareLink, name='shareLink'),
-    re_path(r'^api/warmup/(?P<backend>[^/]+)/(?P<target>[a-zA-Z0-9_-]+)$', views.warmup, name='warmup'),
-    re_path(r'^api/examples/(?P<backend>[^/]+)$', views.examples, name='examples'),
+        name="index",
+    ),
+    re_path(r"^api/share$", views.shareLink, name="shareLink"),
+    re_path(
+        r"^api/warmup/(?P<backend>[^/]+)/(?P<target>[a-zA-Z0-9_-]+)$",
+        views.warmup,
+        name="warmup",
+    ),
+    re_path(r"^api/examples/(?P<backend>[^/]+)$", views.examples, name="examples"),
+    re_path(r"^api/prefixes/(?P<backend>[^/]+)$", views.prefixes, name="prefixes"),
 ]

commit d4e356a66adba2bfa7e90cbe44cebf3d98555b99

Fix bug introduced in previous commit (#118)
Due to a variable renaming, the `examples` API did not work anymore.
This is now fixed.

backend/views.py
@@ -188,7 +188,7 @@ def examples(request, backend):
         examples_tsv = command.handle(returnLog=True, slug=[backend])
     except Exception as e:
         return HttpResponse("Error: " + str(e), status=500)
-    return HttpResponse(tsv, content_type="text/tab-separated-values")
+    return HttpResponse(examples_tsv, content_type="text/tab-separated-values")
 
 
 # Handle API request like https://qlever.cs.uni-freiburg.de/api/prefixes/wikidata