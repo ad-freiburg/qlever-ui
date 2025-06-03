d76b511b555888dcde3c460c2c667ddf4798291d
Ioannis Nezis <ioannis@nezis.de>
2025-06-03 18:07:45 +0200

    connect to example selection

--- monaco_editor/qleverUi.ts
+++ monaco_editor/qleverUi.ts
@@ -1,10 +1,18 @@
 import { MonacoEditorLanguageClientWrapper } from "monaco-editor-wrapper/.";
 
 export function setup_buttons(wrapper: MonacoEditorLanguageClientWrapper) {
-  let formatButton = document.getElementById("formatButton");
-  if (formatButton != undefined) {
-    formatButton.addEventListener("click", () => {
+  // NOTE: Format button
+  document.getElementById("formatButton")!
+    .addEventListener("click", () => {
       wrapper.getEditor()!.trigger("button", "editor.action.formatDocument", {});
     });
-  }
+
+  // NOTE: Query Examples
+  document.querySelectorAll(".queryExample")!
+    .forEach(element => {
+      element.addEventListener("click", () => {
+        wrapper.getEditor()!.setValue(element.getAttribute("value")!)
+        wrapper.getEditor()!.focus();
+      });
+    });
 }
