81c13d949ea620dc34a21d8214f892f0cb6fbbcd
Ioannis Nezis <ioannis@nezis.de>
2025-06-16 18:52:52 +0200

    ENH: jump navigation

--- qlever_ui/monaco_editor/keys.ts
+++ qlever_ui/monaco_editor/keys.ts
@@ -7,17 +7,116 @@
 import * as monaco from 'monaco-editor';
 import { MonacoEditorLanguageClientWrapper } from "monaco-editor-wrapper/.";
 import { executeQuery } from '../network/execute';
+import { FormattingResult, JumpResult } from '../types/lsp_messages';
+import { Edit } from '../types/monaco';
 
 export function setup_key_bindings(wrapper: MonacoEditorLanguageClientWrapper) {
+  const editor = wrapper.getEditor()!;
+  const languageClient = wrapper?.getLanguageClient("sparql")!;
 
-	wrapper.getEditor()!.addAction({
-		id: 'Execute Query',
-		label: 'Execute',
-		keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter],
-		contextMenuGroupId: 'navigation',
-		contextMenuOrder: 1.5,
-		run() {
-			executeQuery(wrapper)
-		}
-	});
+  // NOTE: execute query on Ctrl + Enter
+  editor.addAction({
+    id: 'Execute Query',
+    label: 'Execute',
+    keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter],
+    contextMenuGroupId: 'navigation',
+    contextMenuOrder: 1.5,
+    run() {
+      executeQuery(wrapper)
+    }
+  });
+
+  // NOTE:jump to next or prev position (Alt + n, Alt + p)
+  monaco.editor.addCommand({
+    id: 'jumpToNextPosition',
+    run: (_get, args) => {
+      console.log(args);
+      // NOTE: Format document
+      languageClient
+        .sendRequest('textDocument/formatting', {
+          textDocument: { uri: editor.getModel()!.uri.toString() },
+          options: {
+            tabSize: 2,
+            insertSpaces: true
+          }
+        })
+        .then((response) => {
+          const jumpResult = response as FormattingResult;
+          const edits: Edit[] = jumpResult.map((edit) => {
+            return {
+              range: new monaco.Range(
+                edit.range.start.line + 1,
+                edit.range.start.character + 1,
+                edit.range.end.line + 1,
+                edit.range.end.character + 1
+              ),
+              text: edit.newText
+            };
+          });
+          editor.getModel()!.applyEdits(edits);
+
+          // NOTE: request jump position
+          const cursorPosition = editor.getPosition()!;
+          languageClient
+            .sendRequest('qlueLs/jump', {
+              textDocument: { uri: editor.getModel()?.uri.toString() },
+              position: {
+                line: cursorPosition.lineNumber - 1,
+                character: cursorPosition.column - 1
+              },
+              previous: args === 'prev'
+            })
+            .then((response) => {
+              // NOTE: move cursor
+              if (response) {
+
+                const typedResponse = response as JumpResult;
+                const newCursorPosition = {
+                  lineNumber: typedResponse.position.line + 1,
+                  column: typedResponse.position.character + 1
+                };
+                if (typedResponse.insertAfter) {
+                  editor.executeEdits('jumpToNextPosition', [
+                    {
+                      range: new monaco.Range(
+                        newCursorPosition.lineNumber,
+                        newCursorPosition.column,
+                        newCursorPosition.lineNumber,
+                        newCursorPosition.column
+                      ),
+                      text: typedResponse.insertAfter
+                    }
+                  ]);
+                }
+                editor.setPosition(newCursorPosition, 'jumpToNextPosition');
+                if (typedResponse.insertBefore) {
+                  editor.getModel()?.applyEdits([
+                    {
+                      range: new monaco.Range(
+                        newCursorPosition.lineNumber,
+                        newCursorPosition.column,
+                        newCursorPosition.lineNumber,
+                        newCursorPosition.column
+                      ),
+                      text: typedResponse.insertBefore
+                    }
+                  ]);
+                }
+                editor.trigger('editor', 'editor.action.triggerSuggest', {});
+              }
+            });
+        });
+      editor.trigger('jumpToNextPosition', 'editor.action.formatDocument', {});
+    }
+  });
+  monaco.editor.addKeybindingRule({
+    command: 'jumpToNextPosition',
+    commandArgs: 'next',
+    keybinding: monaco.KeyMod.Alt | monaco.KeyCode.KeyN
+  });
+  monaco.editor.addKeybindingRule({
+    command: 'jumpToNextPosition',
+    commandArgs: 'prev',
+    keybinding: monaco.KeyMod.Alt | monaco.KeyCode.KeyP
+  });
 }
--- qlever_ui/types/lsp_messages.ts
+++ qlever_ui/types/lsp_messages.ts
@@ -1,3 +1,21 @@
+export interface Position {
+  line: number,
+  character: number
+}
+
+export interface Range {
+  start: Position,
+  end: Position
+}
+
+
+export interface TextEdit {
+  range: Range,
+  newText: string,
+}
+
+export type FormattingResult = TextEdit[];
+
 export interface IdentifyOperationTypeResult {
   operationType: OperationType
 }
@@ -5,4 +23,10 @@ export enum OperationType {
   Query = "Query",
   Update = "Update",
   Unknown = "Unknown"
+}
+
+export interface JumpResult {
+  position: Position,
+  insertBefore?: string,
+  insertAfter?: string,
 }
