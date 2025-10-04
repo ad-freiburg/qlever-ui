b3ea7986408f45bf1cde1204eb882bb3807a81a1
Ioannis Nezis <ioannis@nezis.de>
2025-10-04 23:12:55 +0200

    redo formatting

--- backend/models.py
+++ backend/models.py
@@ -390,7 +390,9 @@ class Backend(models.Model):
             setattr(
                 self,
                 field,
-                str(getattr(self, field)).replace("\r\n", "\n").replace("\r", "\n"),
+                str(getattr(self, field))
+                .replace("\r\n", "\n")
+                .replace("\r", "\n"),
             )
         super(Backend, self).save(*args, **kwargs)
 
@@ -548,7 +550,9 @@ class BackendDefaults(Backend):
 
     def save(self, *args, **kwargs):
         self.name = "Global defaults for all Backends"
-        self.slug = "globaldefaults_" + str(datetime.datetime.now().timestamp())
+        self.slug = "globaldefaults_" + str(
+            datetime.datetime.now().timestamp()
+        )
         self.sortKey = "0"
         self.baseUrl = ""
         self.isDefault = False
