04ed3629a9ccdf5f1576a97b132a641ec3f3f94c
Ioannis Nezis <ioannis@nezis.de>
2025-10-06 14:11:41 +0200

    load backend config from API

--- qlever_ui/backend/backends.ts
+++ qlever_ui/backend/backends.ts
@@ -7,17 +7,50 @@
 import { MonacoEditorLanguageClientWrapper } from "monaco-editor-wrapper/.";
 import { BackendConfig } from "../types/backend";
 import yaml from 'yaml';
-import backendConfigurations from "./backends.yaml?raw";
 
-declare const SLUG: string;
+export async function configure_backends(wrapper: MonacoEditorLanguageClientWrapper) {
+  const slug = window.location.pathname.substr(1).split("/")[0];
 
-export function configure_backends(wrapper: MonacoEditorLanguageClientWrapper) {
-  const backends = yaml.parse(backendConfigurations)
-  backends.forEach((backend) => {
-    backend.default = backend.backend.slug === SLUG;
-    addBackend(wrapper, backend);
+  const backends = await fetch("/api/backends/")
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      return json
+    }).catch(err => {
+      console.error("An error occured while fetching backends\n", err);
+    });
+
+  configureBackendsSelector(wrapper, backends, slug);
+
+
+  backends.forEach(async (backend) => {
+    const config = await fetch(backend.url)
+      .then(response => response.json())
+      .then(json => {
+        const backend = {
+          name: json.name,
+          slug: json.slug,
+          url: json.baseUrl,
+        }
+        const prefixMap = json["prefix_map"];
+        const queries = {
+          subjectCompletion: json["suggestSubjectsContextInsensitive"],
+          predicateCompletion: json["suggestObjectsContextInsensitive"],
+          objectCompletion: json["suggestObjectsContextInsensitive"],
+          predicateCompletionContextSensitive: json["suggestPredicates"],
+          objectCompletionContextSensitive: json["suggestObjects"]
+        };
+        return {
+          backend: backend,
+          prefixMap: prefixMap,
+          queries: queries,
+          default: backend.slug === slug
+        }
+      });
+    addBackend(wrapper, config);
   });
-  configureBackendsSelector(wrapper, backends);
+
 }
 
 function addBackend(wrapper: MonacoEditorLanguageClientWrapper, conf: BackendConfig) {
@@ -30,18 +63,18 @@ function addBackend(wrapper: MonacoEditorLanguageClientWrapper, conf: BackendCon
 }
 
 
-export function configureBackendsSelector(wrapper: MonacoEditorLanguageClientWrapper, backendConfigurations: BackendConfig[]) {
+export function configureBackendsSelector(wrapper: MonacoEditorLanguageClientWrapper, backendConfigurations, slug) {
   const backendsSelectionList = document.getElementById("backendSelectionList")!;
   const backendsSelectionListButton = document.getElementById("backendDisplay")!;
-  backendConfigurations.forEach((backendConfiguration) => {
+  backendConfigurations.forEach((backend) => {
     const a = document.createElement("a");
-    a.textContent = backendConfiguration.backend.name;
-    a.href = `/${backendConfiguration.backend.slug}`;
+    a.textContent = backend.name;
+    a.href = `/${backend.slug}`;
     const li = document.createElement("li");
     li.appendChild(a);
     backendsSelectionList.appendChild(li);
-    if (backendConfiguration.backend.slug === SLUG) {
-      backendsSelectionListButton.textContent = backendConfiguration.backend.name;
+    if (backend.slug === slug) {
+      backendsSelectionListButton.textContent = backend.name;
     }
   });
 }
